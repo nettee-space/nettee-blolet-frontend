e0de4ff13e9cd24a6ccf6f32c6752a4462f0f94e
kimdonggu42 <kdk3280@undermilli.com>
2025-07-17 13:24:06 +0900

    chore: prettier 세팅

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <div className='min-h-screen flex items-center justify-center'>
+    <div className='flex min-h-screen items-center justify-center'>
       <main className='text-center'>
-        <h1 className='text-4xl font-bold mb-4'>Blolet</h1>
+        <h1 className='mb-4 text-4xl font-bold'>Blolet</h1>
       </main>
     </div>
   );
